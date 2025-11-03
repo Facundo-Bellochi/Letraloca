621211f0edca8815cf73a5ba5717306f4a640f36
santiago leffler <santileffler.123@gmail.com>
2025-11-02 22:38:20 -0300

    resuelto lo del muñeco

--- interfaz.py
+++ interfaz.py
@@ -1,4 +1,5 @@
 import os 
+
 def bienvenida():
     print("Bienvenido al Juego del Ahorcado!")
     input("Presioná ENTER para comenzar...")
@@ -20,80 +21,71 @@ def mostrar_estado(palabra_secreta, letras_correctas, letras_incorrectas, intent
         f"Errores: {intentos} / 6\n"
         f"Palabra: {estado_palabra}\n"
         f"Letras incorrectas: {', '.join(letras_incorrectas) if letras_incorrectas else 'Ninguna'}\n"
-        + "_" * 30
+        + "_" * 30 + "\n\n\n"
     )
     dibujo = obtener_dibujo(intentos)
     imprimir_dos_columnas(texto_izq, dibujo)
 
 def obtener_dibujo(intentos):
     dibujos = [
-        # 0 errores
-        """ 
-    +---+
-        |
-
-        
-        
-    =========
+        """\
++---+
+|   |
+|   O
+|  /|\\
+|  / \\
+=========
 """,
-        # 1 error
-        """ 
-    +---+
-        |
-        O
-        
-        
-    =========
+        """\
++---+
+|   |
+|   O
+|  /|\\
+|  / 
+=========
 """,
-        # 2 errores
-        """ 
-    +---+
-    |   |
-        O
-        |
-        
-    =========
+        """\
++---+
+|   |
+|   O
+|  /|\\
+|    
+=========
 """,
-        # 3 errores
-        """ 
-    +---+
-    |   |
-    |   O
-       /|
-        
-    =========
+        """\
++---+
+|   |
+|   O
+|  /|
+|    
+=========
 """,
-        # 4 errores
-        """ 
-    +---+
-    |   |
-    |   O
-    |  /|\\
-        
-    =========
+        """\
++---+
+|   |
+|   O
+|   |
+|    
+=========
 """,
-        # 5 errores
-        """ 
-    +---+
-    |   |
-    |   O
-    |  /|\\
-       /
-    =========
+        """\
++---+
+|   |
+|   O
+|    
+|    
+=========
 """,
-        # 6 errores
-        """ 
-    +---+
-    |   |
-    |   O
-    |  /|\\
-    |  / \\
-    =========
+        """\
++---+
+|   |
+|    
+|    
+|    
+=========
 """
     ]
-    # Limitar a 6 errores
     return dibujos[min(intentos, 6)]
-
 def imprimir_dos_columnas(texto_izq, dibujo_der):
     izq = texto_izq.split("\n")
     der = dibujo_der.split("\n")
@@ -103,7 +95,7 @@ def imprimir_dos_columnas(texto_izq, dibujo_der):
     for i in range(max_lineas):
         col_izq = izq[i] if i < len(izq) else ""
         col_der = der[i] if i < len(der) else ""
-        print(f"{col_izq:<35} {col_der}")  # 35 = ancho columna izquierda
+        print(f"{col_izq:<50} {col_der}")  # 35 = ancho columna izquierda
 
 
 def pedir_entrada():
